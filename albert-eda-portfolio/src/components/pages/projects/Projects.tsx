f39bf97f4a10f390ecd0ac8686ed5c205dc70cca
Eda Albert <Albert@edas-mbp.home>
2023-10-09 00:01:10 -0400

    add card media

--- albert-eda-portfolio/src/components/pages/projects/Projects.tsx
+++ albert-eda-portfolio/src/components/pages/projects/Projects.tsx
@@ -1,5 +1,6 @@
-import { Modal, Box } from "@mui/material"
+import { Modal, Box, CardMedia } from "@mui/material"
 import { FunctionComponent } from "react"
+import OBSIDI from "../../../assets/obsidi_logo_white.png"
 
 
 interface ProjectsProps {
@@ -12,9 +13,26 @@ export const Projects: FunctionComponent<ProjectsProps> = ({ visible, setVisible
         <Modal 
             open={visible} 
             onClose={() => {setVisible(false)}}
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
         >
-            <Box bgcolor="red">
-                MODAL
+            <Box 
+                bgcolor="#232526" padding="5rem"
+            >
+                <Box width="12.5rem" height="12.5rem" border="4px solid #F4D03F">
+                    <CardMedia 
+                        sx={{
+                            // backgroundColor: 'red',
+                            height: "20%"
+                        }}
+                        component="img"
+                        src={OBSIDI}
+                        title="OBSIDI Logo"
+                    />
+                </Box>
             </Box>
         </Modal>
     )
