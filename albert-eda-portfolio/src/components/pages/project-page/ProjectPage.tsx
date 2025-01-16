5592f619f2664682ea3cdaa306a685cd780d0816
alberteda <alberteda32@gmail.com>
2025-01-16 14:12:48 -0500

    add projects object

--- albert-eda-portfolio/src/components/pages/project-page/ProjectPage.tsx
+++ albert-eda-portfolio/src/components/pages/project-page/ProjectPage.tsx
@@ -1,16 +1,58 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box, Container, Typography, Divider } from "@mui/material";
 import { KeyboardBackspace } from "@mui/icons-material";
 
 const ProjectPage = () => {
+  const projects = [
+    {
+      id: 1,
+      projectName: "San Bruno Group",
+      location: "Ottawa, Ontario, Canada",
+      projectDescription:
+        "San Bruno Group specializes in premium window and glass solutions, offering expert sales, installation, and service for residential and commercial projects.",
+      projectLink: "https://sanbrunogroup.ca/",
+      year: "2024",
+    },
+    {
+      id: 2,
+      projectName: "Dreamland Cafe",
+      location: "Ottawa, Ontario, Canada",
+      projectDescription:
+        "Located in the heart of Ottawa's Little Italy, DreamLand Cafe offers incredible fresh pasta dishes, made with love and finesse.",
+      projectLink: "https://dreamlandcafe.ca/",
+      year: "2024",
+    },
+    {
+      id: 3,
+      projectName: "Ichota",
+      location: "Herrogate, North Yorkshire, UK",
+      projectDescription:
+        "ichota is a dating app designed for busy professionals, helping them connect and build meaningful relationships despite their demanding schedules and limited time to socialize.",
+      projectLink: "https://ichota.co.uk/",
+      year: "2023",
+    },
+    {
+      id: 4,
+      projectName: "Obsidi",
+      location: "Toronto, Ontario, Canada",
+      projectDescription:
+        "Obsidi® is a job board built for Black tech professionals to connect with employers of choice",
+      projectLink: "https://app.obsidi.com/",
+      year: "2022",
+    },
+  ];
+
+  const navigate = useNavigate();
+
   return (
     <Container sx={{ display: "grid", gap: "1rem", paddingTop: "1rem", paddingBottom: "2rem" }}>
       <Box display="flex" alignItems={"center"}>
-        <Box>
+        <Box onClick={() => navigate("/")}>
           <KeyboardBackspace sx={{ color: "white" }} />
         </Box>
         <Box width="100%" display="flex" justifyContent="center">
-          <Typography variant="subtitle2" fontWeight="bold" color={"white"}>
+          <Typography variant="body1" fontWeight="bold" color={"white"}>
             Projects
           </Typography>
         </Box>
@@ -20,6 +62,38 @@ const ProjectPage = () => {
           A showcase of projects I've contributed to over the years — a blend of personal passion and
           professional experience.
         </Typography>
+      </Box>
+      <Box display="flex" gap={"1.5rem"} flexDirection={"column"} alignItems={"center"} mt="2rem">
+        {projects.map((project) => {
+          return (
+            <Box key={project.id} display="flex" width={"100%"} alignItems={"center"} gap={"1rem"}>
+              <Box>
+                <Typography
+                  fontWeight={"bold"}
+                  color={"white"}
+                  textAlign={"left"}
+                  variant="body2"
+                  whiteSpace={"nowrap"}
+                >
+                  {project.projectName}
+                </Typography>
+              </Box>
+              <Box width={"100%"}>
+                <Divider
+                  sx={{
+                    width: "100%",
+                    backgroundColor: "#E2D372",
+                  }}
+                />
+              </Box>
+              <Box>
+                <Typography color={"white"} textAlign={"left"} variant="body2">
+                  {project.year}
+                </Typography>
+              </Box>
+            </Box>
+          );
+        })}
       </Box>
     </Container>
   );
