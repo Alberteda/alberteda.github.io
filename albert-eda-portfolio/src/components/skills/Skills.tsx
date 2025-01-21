c73fd516df37a7915126a5c6ba7d0cb00b64b608
alberteda <alberteda32@gmail.com>
2025-01-21 15:34:03 -0500

    add Skills -component

---
+++ albert-eda-portfolio/src/components/skills/Skills.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Box, CardContent, Chip, Stack, Typography } from "@mui/material";
+import { useSkills } from "../../hooks/use-skills/useSkills";
+
+const Skills = () => {
+  const { skills } = useSkills();
+
+  return (
+    <CardContent
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        "&.MuiCardContent-root	": {
+          padding: "0 !important",
+        },
+      }}
+    >
+      <Box display={"flex"} flexDirection="column" gap="1.5rem">
+        <Box display={"flex"} flexDirection="column" gap="1rem">
+          <Typography variant="body1" color={"white"} textAlign={"left"} fontWeight={"700"}>
+            Programming Languages
+          </Typography>
+          <Box overflow={"auto"} sx={{ scrollbarWidth: "none", "&::-webkit-scrollbar": { display: "none" } }}>
+            {skills && (
+              <Stack direction="row" gap="0.5rem">
+                {skills.map(({ skill, value }, index) => {
+                  return (
+                    skill === "language" && (
+                      <Chip
+                        key={index}
+                        label={value}
+                        variant="outlined"
+                        sx={{ backgroundColor: "#4B8063", color: "white", fontWeight: "700" }}
+                      />
+                    )
+                  );
+                })}
+              </Stack>
+            )}
+          </Box>
+        </Box>
+        <Box display={"flex"} flexDirection="column" gap="1rem">
+          <Typography variant="body1" color={"white"} textAlign={"left"} fontWeight={"700"}>
+            Frameworks{" "}
+          </Typography>
+          <Box overflow={"auto"} sx={{ scrollbarWidth: "none", "&::-webkit-scrollbar": { display: "none" } }}>
+            {skills && (
+              <Stack direction="row" gap="0.5rem">
+                {skills.map(({ skill, value }, index) => {
+                  return (
+                    skill === "framework" && (
+                      <Chip
+                        key={index}
+                        label={value}
+                        variant="outlined"
+                        sx={{ backgroundColor: "#4B8063", color: "white", fontWeight: "700" }}
+                      />
+                    )
+                  );
+                })}
+              </Stack>
+            )}
+          </Box>
+        </Box>
+        <Box display={"flex"} flexDirection="column" gap="1rem">
+          <Typography variant="body1" color={"white"} textAlign={"left"} fontWeight={"700"}>
+            Tools
+          </Typography>
+          <Box overflow={"auto"} sx={{ scrollbarWidth: "none", "&::-webkit-scrollbar": { display: "none" } }}>
+            {skills && (
+              <Stack direction="row" gap="0.5rem">
+                {skills.map(({ skill, value }, index) => {
+                  return (
+                    skill === "tool" && (
+                      <Chip
+                        key={index}
+                        label={value}
+                        variant="outlined"
+                        sx={{ backgroundColor: "#4B8063", color: "white", fontWeight: "700" }}
+                      />
+                    )
+                  );
+                })}
+              </Stack>
+            )}
+          </Box>
+        </Box>
+      </Box>
+    </CardContent>
+  );
+};
+
+export default Skills;
