c402b66c6537e1e9f609485a46bee362827ce9bc
alberteda <alberteda32@gmail.com>
2025-01-20 23:21:48 -0500

    add projects component

--- albert-eda-portfolio/src/App.tsx
+++ albert-eda-portfolio/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import { LandingPage } from "./components/pages/landing-page/LandingPage";
-import ProjectPage from "./components/pages/project-page/ProjectPage"; // Ensure this path is correct and the file exists
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from "./hooks/use-theme/usetheme";
 import { ThemeProvider } from "@mui/material";
+import { LandingPage } from "./components/pages/landing-page/LandingPage";
+import ProjectPage from "./components/pages/project-page/ProjectPage";
+import Projects from "./components/pages/project/Projects";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/experience" element={<ProjectPage />} />
+          <Route path="/project" element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- albert-eda-portfolio/src/components/pages/project/Projects.tsx
+++ albert-eda-portfolio/src/components/pages/project/Projects.tsx
@@ -1,7 +1,7 @@
 import {
-  Dialog,
   Box,
   Typography,
+  Container,
   Button,
   Card,
   CardHeader,
@@ -15,99 +15,54 @@ import OBSIDI from "../../../assets/obsidi_logo_white.png";
 import GitHub from "../../../assets/github.svg";
 import styled from "styled-components";
 
-export const StyledSocials = styled.img`
-  width: 2rem;
-  height: 2rem;
-  object-fit: cover;
-  object-position: center;
-  border-radius: 50%;
-`;
-
-export const StyledDivider = styled(Divider)`
-  width: 30%;
-  height: 0.1rem;
-  background-color: #8aaae5;
-  border-radius: 20px;
-`;
-
-export const StyledDialog = styled(Dialog)`
-  && {
-    // increase specificity to override the default styles
-    .MuiDialog-paper {
-      border-radius: 20px;
-    }
-  }
-`;
-
-interface ProjectsProps {
-  visible: boolean;
-  setVisible: (value: boolean | ((prevValue: boolean) => boolean)) => void; // accepts a setState function
-}
-
-export const Project: FunctionComponent<ProjectsProps> = ({ visible, setVisible }) => {
+const Projects = () => {
   return (
-    <StyledDialog
-      open={visible}
-      keepMounted
-      onClose={() => {
-        setVisible(false);
-      }}
+    <Container
       sx={{
+        textAlign: "left",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        flexDirection: "column",
+        gap: "2rem",
       }}
-      fullWidth
     >
-      <Box bgcolor="#232526" padding="2rem">
-        <Box mb="2rem">
-          <Typography variant="h5" color="white" fontWeight="bolder">
-            PROJECTS
-          </Typography>
-          <StyledDivider />
+      <Box display={"flex"} justifyContent={"space-around"}>
+        <Box>
+          <Typography>Title</Typography>
         </Box>
-        <Box display="flex" padding="1rem 0">
-          <Box>
-            <Card
-              sx={{
-                backgroundColor: "#232526",
-                maxWidth: "30rem",
-                paddingBottom: "1rem",
-                boxShadow: "2px 2px 5px #BB86FC",
-              }}
-            >
-              <CardHeader
-                avatar={<Avatar src={OBSIDI} sx={{ width: "8rem", height: "auto", borderRadius: "0" }} />}
-              />
-              <CardContent>
-                <Typography color="white" variant="body1" fontWeight="bold">
-                  An employment-focused, multi-sided marketplace that enables access to networking and future
-                  job opportunities Black professionals globally.
-                </Typography>
-                <Typography color="white" variant="body2" mt="1rem" letterSpacing="2px">
-                  React, Typescript, Node JS, PostgreSQL, Postman, Dockers
-                </Typography>
-              </CardContent>
-              <CardActions sx={{ display: "flex", gap: "1rem", paddingLeft: "1rem" }}>
-                <a href="https://github.com/albert-eda-bptn" target="_blank" rel="noopener noreferrer">
-                  <StyledSocials src={GitHub} />
-                </a>
-                <Button
-                  size="small"
-                  href="https://app.obsidi.com/"
-                  target="_blank"
-                  rel="noopener noreferrer" // security: protects the processing of opening a new tab, disabling the windows opener and referer properties
-                  style={{ fontWeight: "bold", color: "white" }}
-                >
-                  Learn More
-                </Button>
-              </CardActions>
-            </Card>
-          </Box>
+        <Box>
+          <Typography>Project Date</Typography>
         </Box>
       </Box>
-    </StyledDialog>
+
+      <Box>
+        <Typography>Project Sanpshot</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Role</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Team</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Context</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Features & Mechanics</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Technologies</Typography>
+      </Box>
+
+      <Box>
+        <Typography>Project Links</Typography>
+      </Box>
+    </Container>
   );
 };
 
-export default Project;
+export default Projects;
