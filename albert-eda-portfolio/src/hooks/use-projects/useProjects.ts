dd6b6a9280d3069c4fc30d373892454acfffe069
alberteda <alberteda32@gmail.com>
2025-01-21 15:34:57 -0500

    improve projects object

--- albert-eda-portfolio/src/hooks/use-projects/useProjects.ts
+++ albert-eda-portfolio/src/hooks/use-projects/useProjects.ts
@@ -2,7 +2,12 @@ const projects = [
   {
     id: 1,
     projectName: "Obsidi",
+    Role: `Full-stack Mobile & Web Developer`,
+    Team: `Team of 5`,
     location: "Toronto, Ontario, Canada",
+    technology: ` TypeScript, React, React Native, Tailwind, NativeWind UI, PostgreSQL, Node.js, Knex.js, REST APIs, 
+Postman, Postico, Docker, AWS, GitHub, GitHub Actions, Figma, Asana
+`,
     projectDescription:
       "Obsidi® is a job board built for Black tech professionals to connect with employers of choice",
     projectLink: "https://app.obsidi.com/",
@@ -11,16 +16,24 @@ const projects = [
   {
     id: 2,
     projectName: "Ichota",
+    Role: `Full-stack Mobile & Web Developer`,
+    Team: `Team of 2`,
     location: "Herrogate, North Yorkshire, UK",
+    technology: ` TypeScript, React, React Native, Tailwind, NativeWind UI, PostgreSQL, Node.js, Knex.js, REST APIs, 
+Postman, Postico, Docker, AWS, GitHub, GitHub Actions, Figma, Asana
+`,
     projectDescription:
-      "ichota is a dating app designed for busy professionals, helping them connect and build meaningful relationships despite their demanding schedules and limited time to socialize.",
+      "Ichota is a dating app designed for busy professionals, helping them connect and build meaningful relationships despite their demanding schedules and limited time to socialize.",
     projectLink: "https://ichota.co.uk/",
     year: "2024 - Present",
   },
   {
     id: 3,
     projectName: "San Bruno Group",
+    Role: `Developer, Designer & Researcher`,
+    Team: `Solo Project`,
     location: "Ottawa, Ontario, Canada",
+    technology: `TypeScript, React, Vite, Material UI, Email JS, Netlify, Cloudfare, GitHub, Figma, Asana`,
     projectDescription:
       "San Bruno Group specializes in premium window and glass solutions, offering expert sales, installation, and service for residential and commercial projects.",
     projectLink: "https://sanbrunogroup.ca/",
@@ -29,7 +42,10 @@ const projects = [
   {
     id: 4,
     projectName: "Dreamland Cafe",
+    Role: `Developer, Designer & Researcher`,
+    Team: `Solo Project`,
     location: "Ottawa, Ontario, Canada",
+    technology: `TypeScript, React, React Native, Material UI, OpenTable, Square, GitHub, GitHub Actions, Netlify, Cloudfare, Figma, Asana`,
     projectDescription:
       "Located in the heart of Ottawa's Little Italy, DreamLand Cafe offers incredible fresh pasta dishes, made with love and finesse.",
     projectLink: "https://dreamlandcafe.ca/",
