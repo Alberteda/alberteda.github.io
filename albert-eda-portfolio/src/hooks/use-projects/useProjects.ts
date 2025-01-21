ed954d61cfada775bea83ebc42ad76ec9cac2897
alberteda <alberteda32@gmail.com>
2025-01-20 23:22:18 -0500

    useProjects

---
+++ albert-eda-portfolio/src/hooks/use-projects/useProjects.ts
@@ -0,0 +1,40 @@
+const projects = [
+  {
+    id: 1,
+    projectName: "Obsidi",
+    location: "Toronto, Ontario, Canada",
+    projectDescription:
+      "Obsidi® is a job board built for Black tech professionals to connect with employers of choice",
+    projectLink: "https://app.obsidi.com/",
+    year: "2022 - Present",
+  },
+  {
+    id: 2,
+    projectName: "Ichota",
+    location: "Herrogate, North Yorkshire, UK",
+    projectDescription:
+      "ichota is a dating app designed for busy professionals, helping them connect and build meaningful relationships despite their demanding schedules and limited time to socialize.",
+    projectLink: "https://ichota.co.uk/",
+    year: "2024 - Present",
+  },
+  {
+    id: 3,
+    projectName: "San Bruno Group",
+    location: "Ottawa, Ontario, Canada",
+    projectDescription:
+      "San Bruno Group specializes in premium window and glass solutions, offering expert sales, installation, and service for residential and commercial projects.",
+    projectLink: "https://sanbrunogroup.ca/",
+    year: "2024",
+  },
+  {
+    id: 4,
+    projectName: "Dreamland Cafe",
+    location: "Ottawa, Ontario, Canada",
+    projectDescription:
+      "Located in the heart of Ottawa's Little Italy, DreamLand Cafe offers incredible fresh pasta dishes, made with love and finesse.",
+    projectLink: "https://dreamlandcafe.ca/",
+    year: "2024",
+  },
+];
+
+export default projects;
