31a5bd58fc53b1dbf7a11a8fd957010876a31ba9
alberteda <alberteda32@gmail.com>
2025-01-22 20:56:10 -0500

    update properties

--- albert-eda-portfolio/src/hooks/use-projects/useProjects.ts
+++ albert-eda-portfolio/src/hooks/use-projects/useProjects.ts
@@ -2,11 +2,10 @@ const projects = [
   {
     id: 1,
     projectName: "Obsidi",
-    Role: `Full-stack Mobile & Web Developer`,
-    Team: `Team of 5`,
+    role: `Full-stack Mobile & Web Developer`,
+    team: `Team of 5`,
     location: "Toronto, Ontario, Canada",
-    technology: ` "TypeScript", "React", "React Native", "Tailwind", "NativeWind UI", "PostgreSQL", "Node.js", "Knex.js", "REST APIs", 
-"Postman", "Postico", "Docker", "AWS", "GitHub", "GitHub Actions", "Figma", "Asana"
+    technology: `TypeScript,React,React Native,Tailwind,NativeWind UI,PostgreSQL,Node.js,Knex.js,REST APIs,Postman,Postico,Docker,AWS,GitHub,GitHub Actions,Figma,Asana
 `,
     projectDescription:
       "Obsidi® is a job board built for Black tech professionals to connect with employers of choice",
@@ -16,12 +15,10 @@ const projects = [
   {
     id: 2,
     projectName: "Ichota",
-    Role: `Full-stack Mobile & Web Developer`,
-    Team: `Team of 2`,
+    role: `Full-stack Mobile & Web Developer`,
+    team: `Team of 2`,
     location: "Herrogate, North Yorkshire, UK",
-    technology: ` "TypeScript", "React", "React Native", "Tailwind", "NativeWind UI", "PostgreSQL", "Node.js", "Knex.js", "REST APIs", 
-"Postman", "Postico", "Docker", "AWS", "GitHub", "GitHub Actions", "Figma", "Asana"
-`,
+    technology: `TypeScript,React,React Native,Tailwind,NativeWind UI,PostgreSQL,Node.js,Knex.js,REST APIs,Postman,Postico,Docker,AWS,GitHub,GitHub Actions,Figma,Asana`,
     projectDescription:
       "Ichota is a dating app designed for busy professionals, helping them connect and build meaningful relationships despite their demanding schedules and limited time to socialize.",
     projectLink: "https://ichota.co.uk/",
@@ -30,10 +27,10 @@ const projects = [
   {
     id: 3,
     projectName: "San Bruno Group",
-    Role: `Developer, Designer & Researcher`,
-    Team: `Solo Project`,
+    role: `Developer, Designer & Researcher`,
+    team: `Solo Project`,
     location: "Ottawa, Ontario, Canada",
-    technology: `"TypeScript", "React", "Vite", "Material UI", "Email JS", "Netlify", "Cloudfare", "GitHub", "Figma", "Asana"`,
+    technology: `TypeScript,React,Vite,Material UI,Email JS,Netlify,Cloudfare,GitHub,Figma,Asana`,
     projectDescription:
       "San Bruno Group specializes in premium window and glass solutions, offering expert sales, installation, and service for residential and commercial projects.",
     projectLink: "https://sanbrunogroup.ca/",
@@ -42,10 +39,10 @@ const projects = [
   {
     id: 4,
     projectName: "Dreamland Cafe",
-    Role: `Developer, Designer & Researcher`,
-    Team: `Solo Project`,
+    role: `Developer, Designer & Researcher`,
+    team: `Solo Project`,
     location: "Ottawa, Ontario, Canada",
-    technology: `"TypeScript", "React", "React Native", "Material UI", "OpenTable", "Square", "GitHub", "GitHub Actions", "Netlify", "Cloudfare", "Figma", "Asana"`,
+    technology: `TypeScript,React,React Native,Material UI,OpenTable,Square,GitHub,GitHub Actions,Netlify,Cloudfare,Figma,Asana`,
     projectDescription:
       "Located in the heart of Ottawa's Little Italy, DreamLand Cafe offers incredible fresh pasta dishes, made with love and finesse.",
     projectLink: "https://dreamlandcafe.ca/",
