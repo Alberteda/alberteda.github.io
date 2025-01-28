41ce52f9bb9c0d3af88988589524c61570ba00f6
alberteda <alberteda32@gmail.com>
2025-01-27 22:01:27 -0500

    update changes

--- albert-eda-portfolio/vite.config.ts
+++ albert-eda-portfolio/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/albert-eda-portfolio/",
   plugins: [react()],
   server: {
     open: true, // Automatically open the app in the default browser
   },
-})
+});
