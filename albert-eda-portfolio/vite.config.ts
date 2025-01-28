23612d6b23e436bb227ee6836a16e9639bc713aa
alberteda <alberteda32@gmail.com>
2025-01-27 22:22:17 -0500

    remove homepage

--- albert-eda-portfolio/vite.config.ts
+++ albert-eda-portfolio/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/albert-eda-portfolio/",
   plugins: [react()],
   server: {
     open: true, // Automatically open the app in the default browser
